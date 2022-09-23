fa579fd7c237b8eec2b06771702e3f7b59fa8803
Petr Boháč <pbohac@windowslive.com>
2022-09-23 10:34:17 +0200

    identityFramework - basic implementation;

---
+++ vosplzen.sem1h3/Data/Migrations/20220923074721_student.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vosplzen.sem1h3.Data.Migrations
+{
+    public partial class student : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Class",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "AIT2002");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Class",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ vosplzen.sem1h3/Data/Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vosplzen.sem1h3.Data.Model
+{
+    public class Student:IdentityUser
+    {
+        [Required]
+        public string Class { get; set; }
+
+    }
+}
